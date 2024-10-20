1b5e56a3c9222ca18996350a2dd76c14037072b9
kornzhoudev <kornzhou0602@gmail.com>
2024-10-20 16:36:02 +1100

    update

--- lambda/locustfile.py
+++ lambda/locustfile.py
@@ -2,6 +2,9 @@ from locust import HttpUser, task, between
 import json
 
 class LambdaLoadTestUser(HttpUser):
+    # Set the host URL of your API Gateway or Lambda endpoint
+    host = "https://h5q4xxbiv4.execute-api.ap-southeast-2.amazonaws.com/prod"  # Update this URL with your actual endpoint
+
     wait_time = between(1, 2)
 
     @task
